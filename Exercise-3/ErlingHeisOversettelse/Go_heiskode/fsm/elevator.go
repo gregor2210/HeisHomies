d253540528a2215294691934fbfda5326ae9511a
ErlingMS <erling.storronning@gmail.com>
2025-01-27 15:07:20 +0100

    GAMMEL SAVE

--- Exercise-3/ErlingHeisOversettelse/Go_heiskode/fsm/elevator.go
+++ Exercise-3/ErlingHeisOversettelse/Go_heiskode/fsm/elevator.go
@@ -1,20 +1,25 @@
 package fsm
 
-//Direction type. up = 1, down = 0
+import (
+	"Driver-go/elevio"
+)
+
+// Direction type. up = 1, down = 0
 type Direction int
 
 const (
-	Up    Direction = 1
-	Down  Direction = -1
-	Still Direction = 0
+	Up   Direction = 1
+	Down Direction = -1
+	Idle Direction = 0
 )
 
-//Elevator struct containing floor, moving direction and requests
-//is used to keep track of the elevators state
-//is basicly a elevator object
+// Elevator struct containing floor, moving direction and requests
+// is used to keep track of the elevators state
+// is basicly a elevator object
 type Elevator struct {
-	floor              int
-	movingDirection    Direction
+	floor    int
+	motorDir elevio.MotorDirection
+
 	moving_up_stopps   []int
 	moving_dwon_stopps []int
 }
