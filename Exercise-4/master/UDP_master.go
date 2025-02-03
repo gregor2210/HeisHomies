007e84d9ea33d73a4f4f46a2429ea8d77862cc0a
ErlingMS <erling.storronning@gmail.com>
2025-02-03 12:02:11 +0100

    Starten av udp master resever og backup

---
+++ Exercise-4/master/UDP_master.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"time"
+)
+
+func send_udp_message(conn *net.UDPConn, message string) {
+	_, err := conn.Write([]byte(message))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	addr := &net.UDPAddr{
+		IP:   net.ParseIP("localhost"),
+		Port: 8080,
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	num := -1 //deafult number
+	//buffer :=make([]byte, 1024)
+	num_str := strconv.Itoa(num)
+
+	for i := 0; i <= 100; i++ {
+		num_str = strconv.Itoa(i)
+		send_udp_message(conn, num_str)
+		time.Sleep(1 * time.Second)
+		fmt.Println("Sent to server: ", num_str)
+	}
+
+}
