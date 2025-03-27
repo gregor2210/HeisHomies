2e82c0b4cbbe55b76c6f2dd09b32beac35156168
Georg Græsdal <georggraesdal@dhcp-10-22-25-159.wlan.ntnu.no>
2025-03-27 20:24:54 +0100

    samme

--- HeisProsjekt/ErlingHeisOversettelse/Heis1_TCP/elevio/InitIOHandling.go
+++ HeisProsjekt/ErlingHeisOversettelse/Heis1_TCP/elevio/InitIOHandling.go
@@ -7,13 +7,14 @@ func InitIOHandling() (chan ButtonEvent, chan int, chan bool) {
 	drvbuttons := make(chan ButtonEvent)
 	drvFloors := make(chan int)
 	drvObstr := make(chan bool)
+	//drv_stop := make(chan bool)
 
 	go PollButtons(drvbuttons)
 	go PollFloorSensor(drvFloors)
 	go PollObstructionSwitch(drvObstr)
 
 	fmt.Println("IO handling initialized")
-
+	// go PollStopButton(drv_stop) // Ikke implementert enda.
 	return drvbuttons, drvFloors, drvObstr
 
 }
